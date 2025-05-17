597b4e635209fabd146ab1506e15a752760b6858
Vaughan Jones <vlj7491@gmail.com>
2025-05-17 14:37:19 +1000

    chore: Disable fancy tab bar, general config cleanup

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,21 +1,23 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
-
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
+-- Font
 config.font = wezterm.font("SF Mono Powerline")
 config.font_size = 18
 
+-- Window styling
 config.window_decorations = "RESIZE"
-
 config.window_background_opacity = 0.95
 config.macos_window_background_blur = 10
 
+-- Colour scheme
 config.color_scheme = "Oxocarbon Dark (Gogh)"
 
+-- Tabs
 config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
 
+-- Key bindings
 config.keys = {
   {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
   {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
