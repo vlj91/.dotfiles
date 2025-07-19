0c3052df43d2a6a2c98b5b3e512e20b02c294877
Vaughan Jones <vlj7491@gmail.com>
2025-07-19 18:08:55 +1000

    chore: update theme, add ghostty config

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,12 @@
 return {
   { "nyoom-engineering/oxocarbon.nvim" },
   { "cocopon/iceberg.vim" },
+  { "dgox16/oldworld.nvim" },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "iceberg",
+      colorscheme = "oldworld",
     }
   }
 }
