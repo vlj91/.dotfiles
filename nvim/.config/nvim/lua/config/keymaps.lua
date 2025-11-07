32c212c6444543a2b989891339f3b62338199364
Vaughan Jones <vlj7491@gmail.com>
2025-11-07 17:27:39 +1100

    chore: Always find files in git root

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,17 @@
 -- Add any additional keymaps here
 local map = LazyVim.safe_keymap_set
 map("n", "<leader>goB", ":GBrowse<CR>", { desc = "Open line in browser" })
+
+return {
+{
+    "n",
+    "<leader><space>",
+    function()
+      require("telescope.builtin").find_files({
+        cwd = require("lazyvim.util").root(), -- always use git root
+        prompt_title = "Find Files (Git Root)",
+      })
+    end,
+    desc = "Find Files (Git Root)",
+  },
+}
